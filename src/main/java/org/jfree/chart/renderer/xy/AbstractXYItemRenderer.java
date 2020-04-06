b55c7c9fe108df7820f32d7204fd894b2beb1a41
Melanie <melanie.langevin.1@ens.etsmtl.ca>
2020-04-05 20:57:36 -0400

    Extract drawOutlines
    To reduce God Class and Large Method

--- src/main/java/org/jfree/chart/renderer/xy/AbstractXYItemRenderer.java
+++ src/main/java/org/jfree/chart/renderer/xy/AbstractXYItemRenderer.java
@@ -1077,35 +1077,7 @@ public abstract class AbstractXYItemRenderer extends AbstractRenderer
 
             // now draw the outlines, if visible...
             if (im.getOutlinePaint() != null && im.getOutlineStroke() != null) {
-                if (orientation == PlotOrientation.VERTICAL) {
-                    Line2D line = new Line2D.Double();
-                    double y0 = dataArea.getMinY();
-                    double y1 = dataArea.getMaxY();
-                    g2.setPaint(im.getOutlinePaint());
-                    g2.setStroke(im.getOutlineStroke());
-                    if (range.contains(start)) {
-                        line.setLine(start2d, y0, start2d, y1);
-                        g2.draw(line);
-                    }
-                    if (range.contains(end)) {
-                        line.setLine(end2d, y0, end2d, y1);
-                        g2.draw(line);
-                    }
-                } else { // PlotOrientation.HORIZONTAL
-                    Line2D line = new Line2D.Double();
-                    double x0 = dataArea.getMinX();
-                    double x1 = dataArea.getMaxX();
-                    g2.setPaint(im.getOutlinePaint());
-                    g2.setStroke(im.getOutlineStroke());
-                    if (range.contains(start)) {
-                        line.setLine(x0, start2d, x1, start2d);
-                        g2.draw(line);
-                    }
-                    if (range.contains(end)) {
-                        line.setLine(x0, end2d, x1, end2d);
-                        g2.draw(line);
-                    }
-                }
+                drawOutlines(orientation, g2, dataArea, im, range, start2d, end2d);
             }
 
             String label = marker.getLabel();
@@ -1128,6 +1100,33 @@ public abstract class AbstractXYItemRenderer extends AbstractRenderer
                         marker.getLabelTextAnchor());
             }
             g2.setComposite(originalComposite);
+        }
+    }
+
+    private void drawOutlines(PlotOrientation orient, Graphics2D g2, Rectangle2D dataArea, IntervalMarker intervalMarker, Range range,
+                              double start2d, double end2d) {
+        Line2D line = new Line2D.Double();
+        double y0 = dataArea.getMinY();
+        double y1 = dataArea.getMaxY();
+        g2.setPaint(intervalMarker.getOutlinePaint());
+        g2.setStroke(intervalMarker.getOutlineStroke());
+
+        if (range.contains(intervalMarker.getStartValue())) {
+            if (orient == PlotOrientation.VERTICAL) {
+                line.setLine(start2d, y0, start2d, y1);
+            } else {
+                line.setLine(y0, start2d, y1, start2d);
+            }
+
+            g2.draw(line);
+        }
+        if (range.contains(intervalMarker.getEndValue())) {
+            if (orient == PlotOrientation.VERTICAL) {
+                line.setLine(end2d, y0, end2d, y1);
+            }else{
+                line.setLine(y0, end2d, y1, end2d);
+            }
+            g2.draw(line);
         }
     }
 
